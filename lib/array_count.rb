4ea1eca3d31d4acd5e9c3a385f0fcf6f48590b2d
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 20:20:58 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,6 +4,6 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  array.count_empty_strings
+
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
